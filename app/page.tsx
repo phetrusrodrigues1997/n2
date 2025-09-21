c4ab4dce3277ff918eff6ab7c017741cd12a42f8
Phetrus <phetrusrodrigues@gmail.com>
2025-09-21 10:36:14 +0100

    header padding reduced on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -523,7 +523,7 @@ export default function App() {
       {/* Hide header and all content when LandingPage is loading or showing coming soon */}
       {!isLandingPageLoading && activeSection !== 'comingsoon' && (
         <header
-  className={`z-50 bg-white md:py-2 sticky top-0 overflow-x-hidden  ${
+  className={`z-50 bg-white py-0 md:py-2 sticky top-0 overflow-x-hidden  ${
     (activeSection === "home") ? "border-b border-gray-200" : ""
   }`}
 >
@@ -738,7 +738,7 @@ export default function App() {
 
           {/* Market Carousel - only show on home and dashboard sections, on its own line */}
           {(activeSection === 'home') && (
-            <div className="mt-4 md:mt-1 md:translate-y-2 pt-1 md:pt-0 px-5 md:px-8 ">
+            <div className=" mt-1 md:translate-y-2 pt-1 md:pt-0 px-5 md:px-8 ">
               {/* Markets Container - Show first 13 on desktop, all on mobile */}
               <div className="flex overflow-x-auto md:overflow-visible scrollbar-hide pb-1"
                 style={{
